bff65cab5c23b09b03e923ed8e30fe2a438cade0
noahspott <noahspottdev@gmail.com>
2025-06-14 21:39:04 -0300

    feat: Menu and ExportSection components

---
+++ src/app/binaural-beats-generator/components/menu/Menu.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import logo from "@/public/images/ivoryhaus-logo.png";
+import ExportSection from "./ExportSection";
+
+export default function Menu() {
+  return (
+    <div className="max-w-[216px] p-5 flex flex-col bg-primary-950">
+      <Image src={logo} alt="ivoryhaus logo" className="mb-2" />
+      <div className="bg-primary-900 w-full h-px mb-10" />
+      <ExportSection />
+    </div>
+  );
+}
