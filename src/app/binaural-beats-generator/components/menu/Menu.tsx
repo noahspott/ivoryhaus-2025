949df71d9d767accf3fd4e0347bcddb695dca1f1
noahspott <noahspottdev@gmail.com>
2025-06-15 16:00:00 -0300

    style: add select-none to menu, so nothing can accidentally be highlighted

--- src/app/binaural-beats-generator/components/menu/Menu.tsx
+++ src/app/binaural-beats-generator/components/menu/Menu.tsx
@@ -4,7 +4,7 @@ import ExportSection from "./ExportSection";
 
 export default function Menu() {
   return (
-    <div className="max-w-[216px] p-5 flex flex-col bg-primary-950">
+    <div className="max-w-[216px] p-5 flex flex-col bg-primary-950 select-none">
       <Image src={logo} alt="ivoryhaus logo" className="mb-2" />
       <div className="bg-primary-900 w-full h-px mb-10" />
       <ExportSection />
