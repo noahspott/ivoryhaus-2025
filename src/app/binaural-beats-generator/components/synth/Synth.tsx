45c52761ff356cf4067ba051a4080b6455a5500d
noahspott <noahspottdev@gmail.com>
2025-06-16 22:14:56 -0300

    feat: wave selector skeleton, add bg video

--- src/app/binaural-beats-generator/components/synth/Synth.tsx
+++ src/app/binaural-beats-generator/components/synth/Synth.tsx
@@ -1,11 +1,15 @@
 import Header from "./Header";
 import Visualizer from "./Visualizer";
+import WaveformSelector from "./WaveformSelector";
 
 export default function Synth() {
   return (
-    <div className="bg-primary-950 m-6 w-full min-h-full p-3">
+    <div className="bg-primary-950 m-6 w-full min-h-full p-3 select-none">
       <Header />
       <Visualizer className="h-[161px]" />
+      <div className="grid grid-cols-3 gap-3">
+        <WaveformSelector />
+      </div>
     </div>
   );
 }
