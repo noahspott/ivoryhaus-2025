88a2077710062f5e531b6848e57a527f4ec2fe7f
noahspott <noahspottdev@gmail.com>
2025-06-23 21:26:16 -0300

    feat: handle octave and pitch changes w/ Zustand

--- src/stores/synth-store.ts
+++ src/stores/synth-store.ts
@@ -1,22 +1,28 @@
+import { NoteType } from "@tonaljs/note";
 import { createStore } from "zustand/vanilla";
+import { get } from "@tonaljs/note";
 
 export type SynthState = {
   a4Freq: number;
+  note: NoteType;
 };
 
 export type SynthActions = {
   updateA4Freq: (newFrequency: number) => void;
+  updateNote: (newNote: NoteType) => void;
 };
 
 export type SynthStore = SynthState & SynthActions;
 
 export const defaultInitState: SynthState = {
   a4Freq: 440,
+  note: get("C4"),
 };
 
 export const createSynthStore = (initState: SynthState = defaultInitState) => {
   return createStore<SynthStore>()((set) => ({
     ...initState,
     updateA4Freq: (newFrequency) => set(() => ({ a4Freq: newFrequency })),
+    updateNote: (newNote) => set(() => ({ note: newNote })),
   }));
 };
