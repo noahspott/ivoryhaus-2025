95bd60c5e9760da8ec17450810d5f1dd59ee6f35
noahspott <noahspottdev@gmail.com>
2025-08-04 17:10:40 -0300

    feat: beginning of responsive menu

--- src/app/binaural-beats-generator/components/menu/Menu.tsx
+++ src/app/binaural-beats-generator/components/menu/Menu.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import ExportSection from "./ExportSection";
 import PresetsSection from "./PresetsSection";
 import Tooltip from "./Tooltip";
+import { MenuIcon } from "lucide-react";
 
 // Images
 import logo from "@/public/images/ivoryhaus-logo.png";
@@ -15,19 +16,20 @@ export default function Menu({ className }: { className?: string }) {
   return (
     <div
       className={clsx(
-        "p-5 flex flex-col bg-primary-950 select-none",
+        "p-5 flex flex-row xl:flex-col bg-primary-950 select-none justify-between items-center xl:justify-normal",
         className
       )}
     >
-      <Link href="/">
-        <Image src={logo} alt="ivoryhaus logo" className="mb-2" />
+      <Link href="/" aria-label="Navigate to the ivoryHAUS home page">
+        <Image src={logo} alt="ivoryhaus logo" className="mb-2 max-w-40" />
       </Link>
-      <div className="bg-primary-900 w-full h-px mb-10" />
-      <div className="flex flex-col gap-[60px]">
+      <div className="bg-primary-900 w-full h-px mb-10 xl:block hidden" />
+      <div className="flex-col gap-[60px] hidden xl:flex">
         <ExportSection />
         <PresetsSection />
         <Tooltip />
       </div>
+      <MenuIcon className="text-white block xl:hidden" />
     </div>
   );
 }
