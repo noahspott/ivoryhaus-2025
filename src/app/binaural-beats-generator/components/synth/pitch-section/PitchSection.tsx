a4c54274092dad51cff07c4ebcc1d01f9f2e1089
noahspott <noahspottdev@gmail.com>
2025-08-04 17:11:16 -0300

    feat: responsive pitch section

--- src/app/binaural-beats-generator/components/synth/pitch-section/PitchSection.tsx
+++ src/app/binaural-beats-generator/components/synth/pitch-section/PitchSection.tsx
@@ -7,14 +7,30 @@ import PitchSelector from "./PitchSelector";
 import { Music2Icon } from "lucide-react";
 import OctaveSelector from "./OctaveSelector";
 
-export default function PitchSection({ className }: { className?: string }) {
+export default function PitchSection({
+  className,
+  octaveSelectorLayout,
+}: {
+  className?: string;
+  octaveSelectorLayout?: "horizontal" | "vertical";
+}) {
   return (
     <section className={clsx("synth-module flex flex-col gap-6", className)}>
       <IconHeader Icon={Music2Icon} variant="MEDIUM">
         Center Pitch
       </IconHeader>
-      <div className="flex gap-12 self-center">
-        <OctaveSelector />
+      <div
+        className={clsx("flex gap-6 2xl:gap-12 self-center", {
+          "flex-col": octaveSelectorLayout === "horizontal",
+          "flex-row": octaveSelectorLayout === "vertical",
+        })}
+      >
+        <OctaveSelector
+          layout={octaveSelectorLayout}
+          className={clsx({
+            "self-center order-last": octaveSelectorLayout === "horizontal",
+          })}
+        />
         <PitchSelector />
       </div>
     </section>
