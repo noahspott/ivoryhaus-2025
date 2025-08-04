4baf2fb224e6432f0b18b9cac3e2d3a0c6aeefce
noahspott <noahspottdev@gmail.com>
2025-08-04 17:10:16 -0300

    feat: horizontal and vertical layouts for octave selector

--- src/app/binaural-beats-generator/components/synth/pitch-section/OctaveSelector.tsx
+++ src/app/binaural-beats-generator/components/synth/pitch-section/OctaveSelector.tsx
@@ -1,14 +1,29 @@
 // Lib
 import { useSynthStore } from "@/src/providers/synth-store-provider";
+import clsx from "clsx";
 
 // Components
-import { ChevronUp, ChevronDown } from "lucide-react";
+import {
+  ChevronUp,
+  ChevronDown,
+  ChevronLeft,
+  ChevronRight,
+} from "lucide-react";
 import { get, transposeOctaves } from "@tonaljs/note";
 
-export default function OctaveSelector() {
+export default function OctaveSelector({
+  className = "",
+  layout = "vertical",
+}: {
+  layout?: "vertical" | "horizontal";
+  className?: string;
+}) {
   const { note, updateNote } = useSynthStore((state) => state);
   const LOWEST_OCTAVE = 0;
   const HIGHEST_OCTAVE = 8;
+
+  const IncrementIcon = layout === "vertical" ? ChevronUp : ChevronRight;
+  const DecrementIcon = layout === "vertical" ? ChevronDown : ChevronLeft;
 
   function handleButtonClick(action: "INCREMENT" | "DECREMENT") {
     const oldOctave = note.oct ?? 4;
@@ -25,21 +40,42 @@ export default function OctaveSelector() {
   }
 
   return (
-    <div className="text-primary-50 bg-primary-800 border border-primary-700 rounded-full overflow-hidden">
+    <div
+      className={clsx(
+        className,
+        "text-primary-50 bg-primary-800 border border-primary-700 rounded-full overflow-hidden flex flex-col",
+        { "flex-col": layout === "vertical" },
+        { "flex-row items-center": layout === "horizontal" }
+      )}
+    >
       <button
-        className="px-2.5 py-1.5 hover:cursor-pointer group active:scale-95"
+        className={clsx(
+          "px-2.5 py-1.5 hover:cursor-pointer group active:scale-95",
+          { "order-first": layout === "vertical" },
+          { "order-last": layout === "horizontal" }
+        )}
         onClick={() => handleButtonClick("INCREMENT")}
       >
-        <ChevronUp className="h-6 w-6 p-0.5 group-active:scale-95" />
+        <IncrementIcon className="h-6 w-6 p-0.5 group-active:scale-95" />
       </button>
-      <div className="title-medium-emphasized text-center py-3 border-y border-primary-700">
+      <div
+        className={clsx(
+          "title-medium-emphasized text-center border-primary-700",
+          { "border-y py-3": layout === "vertical" },
+          { "border-x px-3": layout === "horizontal" }
+        )}
+      >
         {`C${note.oct}`}
       </div>
       <button
-        className="px-2.5 py-1.5 hover:cursor-pointer group active:scale-95"
+        className={clsx(
+          "px-2.5 py-1.5 hover:cursor-pointer group active:scale-95",
+          { "order-last": layout === "vertical" },
+          { "order-first": layout === "horizontal" }
+        )}
         onClick={() => handleButtonClick("DECREMENT")}
       >
-        <ChevronDown className="h-6 w-6 p-0.5 group-active:scale-95" />
+        <DecrementIcon className="h-6 w-6 p-0.5 group-active:scale-95" />
       </button>
     </div>
   );
